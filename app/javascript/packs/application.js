d298458d7728b7c5d9db4745afbdeedd94ca3b1f
Alexandre Didion <ale.didion@gmail.com>
2020-12-21 19:23:17 +0100

    Add select for the ingredients search but the UI is not really good - to fix"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,9 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'bootstrap';
+import { initSelect2 } from '../components/init_select2';
+
+document.addEventListener('turbolinks:load', () => initSelect2());
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
