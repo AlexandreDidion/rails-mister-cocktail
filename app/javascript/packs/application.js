c6c862775107c28f8d779fe85d450a47208dadb3
Alexandre Didion <ale.didion@gmail.com>
2020-12-27 16:25:12 +0100

    Trying to reveeerse to previous commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,6 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'bootstrap';
-import { initSelect2 } from '../components/init_select2';
-
-document.addEventListener('turbolinks:load', () => initSelect2());
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
