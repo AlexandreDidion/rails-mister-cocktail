56164bc5af9443449e27658ba794fccbfc9f08f5
Alexandre Didion <ale.didion@gmail.com>
2020-12-27 18:12:47 +0100

    Add the review model, routes, controller and incorporate it in the cocktail show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails, only: %i[index show new create] do
     resources :doses, only: %i[new create]
+    resources :reviews, only: %i[new create]
   end
   resources :doses, only: :destroy
+  resources :reviews, only: :destroy
 end
